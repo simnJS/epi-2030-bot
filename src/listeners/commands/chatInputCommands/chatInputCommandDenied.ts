eb38e1974f375536403868540e94a5b32554bfc9
Minilan <minilancraft@gmail.com>
2025-09-11 16:49:57 +0200

    Change message to component

--- src/listeners/commands/chatInputCommands/chatInputCommandDenied.ts
+++ src/listeners/commands/chatInputCommands/chatInputCommandDenied.ts
@@ -1,5 +1,6 @@
 import type { ChatInputCommandDeniedPayload, Events } from '@sapphire/framework';
 import { Listener, UserError } from '@sapphire/framework';
+import { ContainerBuilder, TextDisplayBuilder } from 'discord.js';
 
 export class UserEvent extends Listener<typeof Events.ChatInputCommandDenied> {
 	public override async run({ context, message: content }: UserError, { interaction }: ChatInputCommandDeniedPayload) {
@@ -7,15 +8,18 @@ export class UserEvent extends Listener<typeof Events.ChatInputCommandDenied> {
 		// Use cases for this are for example permissions error when running the `eval` command.
 		if (Reflect.get(Object(context), 'silent')) return;
 
+		const component = [
+				new ContainerBuilder().addTextDisplayComponents(new TextDisplayBuilder().setContent(`### ${content}`))
+			];
+		
 		if (interaction.deferred || interaction.replied) {
 			return interaction.editReply({
-				content,
+				components: component,
 				allowedMentions: { users: [interaction.user.id], roles: [] }
 			});
 		}
-
 		return interaction.reply({
-			content,
+			components: component,
 			allowedMentions: { users: [interaction.user.id], roles: [] },
 			ephemeral: true
 		});
--- src/listeners/commands/messageCommands/messageCommandDenied.ts
+++ src/listeners/commands/messageCommands/messageCommandDenied.ts
@@ -1,5 +1,6 @@
 import type { Events, MessageCommandDeniedPayload } from '@sapphire/framework';
 import { Listener, type UserError } from '@sapphire/framework';
+import { ContainerBuilder, TextDisplayBuilder } from 'discord.js';
 
 export class UserEvent extends Listener<typeof Events.MessageCommandDenied> {
 	public override async run({ context, message: content }: UserError, { message }: MessageCommandDeniedPayload) {
@@ -7,6 +8,10 @@ export class UserEvent extends Listener<typeof Events.MessageCommandDenied> {
 		// Use cases for this are for example permissions error when running the `eval` command.
 		if (Reflect.get(Object(context), 'silent')) return;
 
-		return message.reply({ content, allowedMentions: { users: [message.author.id], roles: [] } });
+		const component = [
+				new ContainerBuilder().addTextDisplayComponents(new TextDisplayBuilder().setContent(`### ${content}`))
+			];
+
+		return message.reply({ components: component, allowedMentions: { users: [message.author.id], roles: [] } });
 	}
 }
