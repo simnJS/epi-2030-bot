0a07b062b97487fcf196cd07045988215dbef276
Minilan <minilancraft@gmail.com>
2025-09-17 22:56:52 +0200

    Deezer + Spotify parse

---
+++ src/commands/Music.ts
@@ -0,0 +1,69 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Awaitable, Command } from '@sapphire/framework';
+import fetch, { Response } from 'node-fetch';
+// @ts-ignore
+import spotifyUrlInfo from "spotify-url-info"
+const { getPreview, getTracks } = spotifyUrlInfo(fetch)
+
+@ApplyOptions<Command.Options>({
+	name: 'music',
+	description: "Get a playable song by providing a link or a title"
+})
+export class PingCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry): Awaitable<void> {
+		registry.registerChatInputCommand(
+			(builder) => 
+				builder
+					.setName(this.name)
+					.setDescription(this.description)
+					.addStringOption((option) => option.setName('link-title').setDescription('The link or the title of the music').setRequired(true)),
+			{guildIds: ['1391769906944409662']}
+		);
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const musicLinkOrTitle = interaction.options.getString('link-title', true);
+		const linkRegex = /\b((https?:\/\/)?(www\.)?[a-zA-Z0-9\-_]+\.[a-zA-Z]{2,}\/\S+)\b/;
+
+		if (linkRegex.test(musicLinkOrTitle)) {
+			let titleAuthor = ''
+			if (/deezer/i.test(musicLinkOrTitle)) {
+				titleAuthor = await deezerParse(musicLinkOrTitle);
+			}
+			else if (/spotify/i.test(musicLinkOrTitle)) {
+				titleAuthor = await spotifyParse(musicLinkOrTitle);
+			}
+			const trackLink = await spotifyLinkGenerator(titleAuthor);
+			interaction.reply(trackLink || 'Impossible de récupérer l’ID');
+		} else {
+			interaction.reply(musicLinkOrTitle);
+		}
+	}
+}
+
+async function deezerParse(shortLink: string): Promise<string> {
+	console.log(shortLink)
+	// https://developers.deezer.com/api
+	const linkRes: Response = await fetch(shortLink, { redirect: 'follow' });
+	const linkData = linkRes.url.split('/').pop() || '';
+	const id = linkData.split('?')[0];
+	console.log(id)
+
+	const response = await fetch('https://api.deezer.com/track/'+ id)
+	const data = await response.json()
+	console.log(data)
+	// duration is recuperable
+	const titleAuthor = `${data.title} ${data.artist.name}`
+	console.log(titleAuthor)
+	return titleAuthor
+;
+}
+
+async function spotifyParse(link: string): Promise<string> {
+  const data = await getPreview(link);
+  return `${data.title} ${data.artist}`;
+}
+
+async function spotifyLinkGenerator(titleAuthor: string): Promise<string>{
+	return titleAuthor
+}
